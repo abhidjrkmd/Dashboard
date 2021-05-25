1abccca457e691b1fdef84d99528c98f7ded5c6f
Amardeep Kumar <AMARDEEP>
2021-05-25 21:11:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+var options = {
+    series: [{
+            name: "Students",
+            data: [18, 20, 30, 44, 55, 57, 56, 61, 58, 63, 60, 66],
+        },
+        {
+            name: " highest Marks",
+            data: [85, 79, 85, 76, 85, 92, 98, 87, 87, 91, 99, 94],
+        },
+
+    ],
+    chart: {
+        type: "bar",
+        height: 250, // make this 250
+        sparkline: {
+            enabled: true, // make this true
+        },
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            columnWidth: "55%",
+            endingShape: "rounded",
+        },
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    stroke: {
+        show: true,
+        width: 2,
+        colors: ["transparent"],
+    },
+    xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    },
+    yaxis: {
+        title: {
+            text: "",
+        },
+    },
+    fill: {
+        opacity: 1,
+    },
+    tooltip: {
+        y: {
+            formatter: function(val) {
+                return val + "%";
+            },
+        },
+    },
+};
+
+var chart = new ApexCharts(document.querySelector("#apex1"), options);
+chart.render();
+
+// Sidebar Toggle Codes;
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+var sidebarCloseIcon = document.getElementById("sidebarIcon");
+
+function toggleSidebar() {
+    if (!sidebarOpen) {
+        sidebar.classList.add("sidebar_responsive");
+        sidebarOpen = true;
+    }
+}
+
+function closeSidebar() {
+    if (sidebarOpen) {
+        sidebar.classList.remove("sidebar_responsive");
+        sidebarOpen = false;
+    }
+}
